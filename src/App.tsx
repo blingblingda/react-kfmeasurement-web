b9bd2863e034f2f2a642bd97c87b1af6cd537da5
YuJi-2023 <sophia.jiyu@gmail.com>
2024-02-15 11:53:25 +1100

    add Slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import MyButton from "./components/Button/Button";
+import Slider from "./components/Slider/Slider";
 import "./App.scss";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <>
       <Header />
       <MyButton onClick={()=>console.log('clicked')}>My Button</MyButton>
+      <Slider />
       <Footer />
     </>
   );
