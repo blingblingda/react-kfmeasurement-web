b187f453cc34639bf1b198d1d62b6f4decfe5112
Belinda Shan <belindashan126@hotmail.com>
2024-02-27 10:58:57 +1100

    Modified the layout of productsPage using grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import ProductsPage from "./components/ProductsPage/ProductsPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
       </Routes>
     </BrowserRouter>
   );
