2be4d26da31c28706bcc850b4e2c25dc430d3412
Belinda Shan <belindashan126@hotmail.com>
2024-02-29 15:20:13 +1100

    modified the style of the ProductCard

--- src/components/UI/Card/ProductCard.tsx
+++ src/components/UI/Card/ProductCard.tsx
@@ -8,11 +8,15 @@ interface ProductCardProps {
 
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
-    <Card className="border-top-line card-min-height">
+    <Card className="card-border-top-line card-min-height">
       <Card.Body className="d-flex flex-column justify-content-between align-items-center">
-        <div>
-          <Card.Title style={{ fontSize: '20px', fontWeight: 'bold', color: 'darkgreen'}}>{product.name}</Card.Title>
-          <Card.Text style={{ fontSize: '12px',  color: 'grey' }}>{product.description}</Card.Text>
+        <div className="text-center">
+          <Card.Title className="fs-5 text-dark-green">
+            {product.name}
+          </Card.Title>
+          <Card.Text className="text-grey px-2">
+            {product.description}
+          </Card.Text>
         </div>
         <Card.Img variant="bottom" src={product.imgSrc} />
         <WebButton btnStyle="dark" onClick={() => console.log("clicked")}>
