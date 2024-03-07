b873d9b8c2a2e83265e95c5cbbcdda8b9d06bf14
Belinda Shan <belindashan126@hotmail.com>
2024-03-07 12:57:33 +1100

    modified ProductDtailsPage

--- src/components/ProductDetailsPage/ProductDetailsPage.tsx
+++ src/components/ProductDetailsPage/ProductDetailsPage.tsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Col, Container, Row, Card } from "react-bootstrap";
+import { Col, Container, Row, Card, Stack } from "react-bootstrap";
 import Footer from "../Layout/Footer/Footer";
 import Header from "../Layout/Header/Header";
 import Items from "../UI/ItemList/itemList";
+import Feature from "../HomePage/Feature";
 import ComponentAbout from "./About";
 import ComponentDetails from "./Details";
 import WebButton from "../UI/Button/WebButton";
 
 const enum Content {
-  About,
-  Details,
+  ABOUT,
+  DETAILS,
 }
 
 const ProductDetailsPage = () => {
   let { productSlug } = useParams();
   const product = Items.filter((item) => item.slug === productSlug)[0];
   const navigate = useNavigate();
-  const [currentContent, setCurrentContent] = useState(Content.About);
+  const [currentContent, setCurrentContent] = useState(Content.ABOUT);
 
   const handleHomeClick: () => void = () => {
     navigate("/");
@@ -37,6 +38,7 @@ const ProductDetailsPage = () => {
   //   event.target.innerText === "About"
   //     ? setCurrentContent(Content.About)
   //     : setCurrentContent(Content.Details);
+  //   console.log(event);
   // };
 
   return (
@@ -83,78 +85,93 @@ const ProductDetailsPage = () => {
             </Col>
           </Row>
         </Container>
-        <div className="bg-white ">
-          <Container className="py-2 px-5 d-flex justify-content-between align-items-center text-dark-green">
-            <div>We can help you install your system.</div>
-            <Row>
-              <Col className="d-flex justify-content-start align-items-center">
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <polygon
-                    fill="#004a60"
-                    fillRule="nonzero"
-                    points="12.250204 15.142857 12.250204 4 11.0563265 4 8 6.43551017 8.77999999 7.45428567 10.9130612 5.75102039 10.9130612 15.142857"
-                    stroke="none"
-                    strokeWidth="1"
-                  ></polygon>
-                </svg>
-                <div className="mx-3" style={{ fontSize: "11px" }}>
-                  Find a trusted installer in your area
-                </div>
+        <div className="bg-white">
+          <Container className="py-2 px-5 text-dark-green ">
+            <Row className="d-flex justify-content-between align-items-center">
+              <Col sm={6} md={6} lg={3} className="fw-bold">
+                We can help you install your system.
               </Col>
-              <Col className="d-flex justify-content-start align-items-center">
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M13.8439761,15.385306 L13.8439761,14.1380393 L9.62245812,14.1380393 L12.5967094,10.1723709 C12.8365684,9.83656838 13.7640231,8.7811889 13.7640231,7.27807265 C13.7640231,5.42316325 12.4208128,4 10.3580256,4 C7.63962393,4 7,6.52651453 7,6.52651453 L8.15132308,6.95826069 C8.15132308,6.95826069 8.58306923,5.24726667 10.3580256,5.24726667 C11.7492077,5.24726667 12.4208128,6.20670257 12.4208128,7.27807265 C12.4208128,8.50934872 11.4134051,9.62869061 10.9976496,10.1723709 L7.06396239,15.385306 L13.8439761,15.385306 Z"
-                    fill="#004a60"
-                    fillRule="nonzero"
-                    stroke="none"
-                    strokeWidth="1"
-                  ></path>
-                </svg>
-                <div className="mx-3" style={{ fontSize: "11px" }}>
-                  Organise an installer to quote for the job
-                </div>
+              <Col lg={6} className="d-none d-lg-block">
+                <Stack direction="horizontal" gap={3}>
+                  <div className="d-flex justify-content-start align-items-center">
+                    <div>
+                      <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <polygon
+                          fill="#004a60"
+                          fillRule="nonzero"
+                          points="12.250204 15.142857 12.250204 4 11.0563265 4 8 6.43551017 8.77999999 7.45428567 10.9130612 5.75102039 10.9130612 15.142857"
+                          stroke="none"
+                          strokeWidth="1"
+                        ></polygon>
+                      </svg>
+                    </div>
+                    <div className="mx-3" style={{ fontSize: "11px" }}>
+                      Find a trusted installer in your area
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-start align-items-center">
+                    <div>
+                      <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M13.8439761,15.385306 L13.8439761,14.1380393 L9.62245812,14.1380393 L12.5967094,10.1723709 C12.8365684,9.83656838 13.7640231,8.7811889 13.7640231,7.27807265 C13.7640231,5.42316325 12.4208128,4 10.3580256,4 C7.63962393,4 7,6.52651453 7,6.52651453 L8.15132308,6.95826069 C8.15132308,6.95826069 8.58306923,5.24726667 10.3580256,5.24726667 C11.7492077,5.24726667 12.4208128,6.20670257 12.4208128,7.27807265 C12.4208128,8.50934872 11.4134051,9.62869061 10.9976496,10.1723709 L7.06396239,15.385306 L13.8439761,15.385306 Z"
+                          fill="#004a60"
+                          fillRule="nonzero"
+                          stroke="none"
+                          strokeWidth="1"
+                        ></path>
+                      </svg>
+                    </div>
+                    <div className="mx-3" style={{ fontSize: "11px" }}>
+                      Organise an installer to quote for the job
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-start align-items-center">
+                    <div>
+                      <svg
+                        width="20"
+                        height="20"
+                        viewBox="0 0 20 20"
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M9.61359789,15.627755 C11.6693336,15.627755 13.2753771,14.3750411 13.2753771,12.3353658 C13.2753771,10.0387236 11.6211523,9.60509184 11.6211523,9.60509184 C11.6211523,9.60509184 13.0023497,8.99479531 13.0023497,7.16390571 C13.0023497,5.38119742 11.4926688,4 9.64571876,4 C7.1884722,4 6.38545044,6.20027961 6.38545044,6.20027961 L7.54180177,6.71421353 C7.54180177,6.71421353 8.03967526,5.25271394 9.64571876,5.25271394 C10.834191,5.25271394 11.6532731,6.16815874 11.6532731,7.16390571 C11.6532731,8.44874052 10.6254053,9.07509749 9.35663093,9.07509749 L9.0193618,9.07509749 L9.0193618,10.311751 L9.54935615,10.311751 C11.1553997,10.311751 11.9263005,11.0826519 11.9263005,12.3353658 C11.9263005,13.411415 11.0590371,14.3750411 9.61359789,14.3750411 C7.67028525,14.3750411 7.1884722,12.528091 7.1884722,12.528091 L6,13.0741458 C6,13.0741458 6.72271958,15.627755 9.61359789,15.627755 Z"
+                          fill="#004a60"
+                          fillRule="nonzero"
+                          stroke="none"
+                          strokeWidth="1"
+                        ></path>
+                      </svg>
+                    </div>
+                    <div className="mx-3" style={{ fontSize: "11px" }}>
+                      Provide market-leading support
+                    </div>
+                  </div>
+                </Stack>
               </Col>
-              <Col className="d-flex justify-content-start align-items-center">
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M9.61359789,15.627755 C11.6693336,15.627755 13.2753771,14.3750411 13.2753771,12.3353658 C13.2753771,10.0387236 11.6211523,9.60509184 11.6211523,9.60509184 C11.6211523,9.60509184 13.0023497,8.99479531 13.0023497,7.16390571 C13.0023497,5.38119742 11.4926688,4 9.64571876,4 C7.1884722,4 6.38545044,6.20027961 6.38545044,6.20027961 L7.54180177,6.71421353 C7.54180177,6.71421353 8.03967526,5.25271394 9.64571876,5.25271394 C10.834191,5.25271394 11.6532731,6.16815874 11.6532731,7.16390571 C11.6532731,8.44874052 10.6254053,9.07509749 9.35663093,9.07509749 L9.0193618,9.07509749 L9.0193618,10.311751 L9.54935615,10.311751 C11.1553997,10.311751 11.9263005,11.0826519 11.9263005,12.3353658 C11.9263005,13.411415 11.0590371,14.3750411 9.61359789,14.3750411 C7.67028525,14.3750411 7.1884722,12.528091 7.1884722,12.528091 L6,13.0741458 C6,13.0741458 6.72271958,15.627755 9.61359789,15.627755 Z"
-                    fill="#004a60"
-                    fillRule="nonzero"
-                    stroke="none"
-                    strokeWidth="1"
-                  ></path>
-                </svg>
-                <div className="mx-3" style={{ fontSize: "11px" }}>
-                  Provide market-leading support
-                </div>
+              <Col sm={6} md={6} lg={3} className="d-flex justify-content-end">
+                <WebButton btnStyle="dark" onClick={handleContactClick}>
+                  Contact Us
+                </WebButton>
               </Col>
             </Row>
-            <div>
-              <WebButton btnStyle="dark" onClick={handleContactClick}>
-                Contact Us
-              </WebButton>
-            </div>
           </Container>
         </div>
+        <Container>
+          <Feature />
+        </Container>
         <div className="bg-white">
           <Container className="pt-4 px-5">
             <Row className="d-flex justify-content-center text-center">
@@ -163,7 +180,7 @@ const ProductDetailsPage = () => {
                 md={6}
                 lg={6}
                 className="bg-danger py-2"
-                onClick={handleContentSwitch(Content.About)}
+                onClick={handleContentSwitch(Content.ABOUT)}
               >
                 About
               </Col>
@@ -172,7 +189,7 @@ const ProductDetailsPage = () => {
                 md={6}
                 lg={6}
                 className="bg-primary py-2"
-                onClick={handleContentSwitch(Content.Details)}
+                onClick={handleContentSwitch(Content.DETAILS)}
               >
                 Details
               </Col>
@@ -181,7 +198,7 @@ const ProductDetailsPage = () => {
         </div>
         <div className="bg-light-grey">
           <Container style={{ height: "500px" }} className="bg-info px-5">
-            {currentContent === Content.About ? (
+            {currentContent === Content.ABOUT ? (
               <ComponentAbout />
             ) : (
               <ComponentDetails />
@@ -189,6 +206,20 @@ const ProductDetailsPage = () => {
           </Container>
         </div>
       </main>
+      <Container className="py-5">
+        <small
+          className="d-inline-block w-50 text-grey "
+          style={{ lineHeight: "1.75" }}
+        >
+          Engineered for Australia's extreme summers & winters. Heat or cool
+          your whole home or a single room with our range of climate solutions.
+          Smart choice cooling. Leading warranties. Easy installation. Leading
+          health features. Made to last. Types: split system air conditioning,
+          multi air conditioning, ducted air conditioning, cassette air
+          conditioning, window air conditioning, gas ducted heating, evaporative
+          cooling
+        </small>
+      </Container>
       <Footer />
     </>
   );
