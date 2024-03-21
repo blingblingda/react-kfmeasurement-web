9803e26d3ee442dc8226ee47cdd82198fc1cf993
YuJi-2023 <sophia.jiyu@gmail.com>
2024-03-21 13:54:19 +1100

    build WhyPage-banner

---
+++ src/components/WhyPage/WhyBanner.tsx
@@ -0,0 +1,38 @@
+import { Col, Container } from "react-bootstrap";
+import whyPageBanner from "../../assets/whyPageBanner.jpg";
+import { useNavigate } from "react-router-dom";
+
+const WhyBanner = () => {
+  const navigate = useNavigate();
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <>
+      <div
+        className="px-4 py-5 text-center"
+        style={{
+          backgroundImage: `url(${whyPageBanner})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "400px",
+        }}
+      >
+        <Col className="py-1 px-4 list-unstyled d-flex justify-content-start">
+          <li className="path-text" onClick={handleHomeClick}>
+            Home
+          </li>
+          <li className="li-before text-gray">Why KFCK</li>
+        </Col>
+        <div className="col-lg-6">
+          <p className="display-7 mt-5 fw-bold text-white">
+          KFCK specializes in the research，development and project design in the field of nuclear instrumentation and industrial on-site measurement and control system. The company has strong talent capabilities and great potential for future development.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WhyBanner;
--- src/components/WhyPage/WhyPage.tsx
+++ src/components/WhyPage/WhyPage.tsx
@@ -4,25 +4,14 @@ import Container from "react-bootstrap/esm/Container";
 import Footer from "../Layout/Footer/Footer";
 import { Col } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import WhyBanner from "./WhyBanner";
 
 const WhyPage = () => {
-  const navigate = useNavigate();
-  const handleHomeClick = () => {
-    navigate("/");
-  };
-
   return (
     <>
       <Header />
       <main className="bg-light-grey">
-        <Container className="px-0">
-          <Col className="py-4 px-4 list-unstyled d-flex justify-content-start">
-            <li className="path-text" onClick={handleHomeClick}>
-              Home
-            </li>
-            <li className="li-before text-dark-green">Why KFCK</li>
-          </Col>
-        </Container>
+        <WhyBanner />
       </main>
       <Footer />
     </>
