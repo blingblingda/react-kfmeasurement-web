3169a49e40eb59cab7f4d0a5fd0869dbb85171fa
YuJi-2023 <sophia.jiyu@gmail.com>
2024-03-26 12:42:27 +1100

    update WhyBanner and WhyJourney

--- src/components/WhyPage/WhyJourney.tsx
+++ src/components/WhyPage/WhyJourney.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import Journey from "../../assets/Journey.jpg"
-import { Stack } from 'react-bootstrap'
+import React from "react";
+import Journey from "../../assets/Journey.jpg";
+import { Stack } from "react-bootstrap";
 
 const WhyJourney = () => {
   return (
     <>
-    <Stack gap={5} className='my-5 text-center text-dark-green'>
-      <h2>26 Years of Research and Progress</h2>
-      <img
-      src={Journey}
-      alt="why journey pic"
-      className="w-100" />
-    </Stack>
+      <Stack gap={5} className="py-5 text-center text-dark-green bg-white">
+        <h2>26 Years of Research and Progress</h2>
+        <img
+          src={Journey}
+          alt="why journey pic"
+          className="w-50 position-relative start-50 translate-middle-x"
+        />
+      </Stack>
     </>
-  )
-}
+  );
+};
 
-export default WhyJourney
+export default WhyJourney;
