04fa8ef785a189ee8186774d877feb1c36740bbc
YuJi-2023 <sophia.jiyu@gmail.com>
2024-04-23 14:58:35 +1000

    refactor WhyPage-Strategy refactor ProductDetailsPage-AboutMainSection create PlateUI for both sections to use

--- src/components/ProductDetailsPage/AboutMainSection.tsx
+++ src/components/ProductDetailsPage/AboutMainSection.tsx
@@ -1,63 +1,27 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { AboutPictures } from "../UI/ItemList/productAboutList";
+import PlateUI from "../UI/Plate/PlateUI";
 
 interface AboutMainSectionProps {
   aboutSrc: [number, number];
 }
 
 const AboutMainSection = ({ aboutSrc }: AboutMainSectionProps) => {
+  const AboutPictureGroup = [];
+  AboutPictureGroup.push(
+    AboutPictures[aboutSrc[0]],
+    AboutPictures[aboutSrc[1]]
+  );
+
   return (
     <>
-      <Container className="grey-sec-space bg-light-grey">
-        <Row className="g-0">
-          <Col lg={8}>
-            <img
-              src={AboutPictures[aboutSrc[0]].pic}
-              alt="First About pic"
-              className="w-100"
-            />
-          </Col>
-          <Col className="bg-white" lg={4}>
-            <div className="p-5">
-              <div>
-                <h3 className="display-6 text-dark-green mb-4">
-                  {AboutPictures[aboutSrc[0]].title}
-                </h3>
-              </div>
-              <div>
-                <span className="col-md-7 fs-6 text-dark-green">
-                  {AboutPictures[aboutSrc[0]].description}
-                </span>
-              </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
-      <Container className="grey-sec-space bg-light-grey">
-        <Row className="g-0">
-          <Col className="bg-white" lg={4}>
-            <div className="p-5">
-              <div>
-                <h3 className="display-6 text-dark-green mb-4">
-                  {AboutPictures[aboutSrc[1]].title}
-                </h3>
-              </div>
-              <div>
-                <span className="col-md-7 fs-6 text-dark-green">
-                  {AboutPictures[aboutSrc[1]].description}
-                </span>
-              </div>
-            </div>
-          </Col>
-          <Col lg={8}>
-            <img
-              src={AboutPictures[aboutSrc[1]].pic}
-              alt="Second About pic"
-              className="w-100"
-            />
-          </Col>
-        </Row>
-      </Container>
+      {AboutPictureGroup.map((item) => (
+        <PlateUI
+          title={item.title}
+          imgSrc={item.pic}
+          description={item.description}
+        />
+      ))}
     </>
   );
 };
